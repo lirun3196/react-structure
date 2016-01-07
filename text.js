35cfbd52b098153b3dec1616f9f97b1e377d935d
lirun <lirun3196@163.com>
2016-01-07 09:04:24 +0800

    add many method

--- text.js
+++ text.js
@@ -1,18 +1,49 @@
 /**
  *@author LiRun lirun@zbj.com
  * @time 2016/1/5 0005 17:58
- * @abstract common input, it's can be string | phone
+ * @abstract common input, it's value can be string | phone
  */
 var React = require('react');
 var ReactDOM = require('react-dom');
 
 var Text = React.createClass({
+    getInitialState: function() {
+        return {
+            className: 'w60p',
+            titleValue: '',
+            placeHolder: '2-30字',
+            maxLength: 30,
+            name: 'key',
+            dataType: 'string'
+        };
+    },
+    // verify : string
+    // verify : phone
+    // verify : empty
+    // errorTip : showError
+    handleErrorTip: function(value){
+
+    },
+    handleTextChange: function(e) {
+        this.setState({titleValue: e.target.value});
+    },
    render: function(){
        return (
            <div className="form-group">
                <label>填写需求名称：</label>
                <div className="form-inline">
-                   <input  className="w60p" id="title" type="text" placeholder="2-30字" maxlength="30"  name="key" value="{%$key%}" autocomplete="off">
+                   <input
+                       id="title"
+                       type="text"
+                       className={this.state.className}
+                       value={this.state.titleValue}
+                       placeholder={this.state.placeHolder}
+                       maxLength={this.state.maxLength}
+                       name={this.state.name}
+                       onChange={this.handleTextChange}
+                       data-type={this.state.dataType}
+                       autoComplete="off"
+                   />
                </div>
            </div>
        );
@@ -20,5 +51,6 @@ var Text = React.createClass({
 });
 
 ReactDOM.render(
+    <Text />,
     document.getElementById('structure')
 );
